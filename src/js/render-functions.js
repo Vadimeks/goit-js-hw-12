4905aab0e1bef5bcf1ccb5db7aa620c675517375
Eks-Vadzim Simanau <eks220885@gmail.com>
2025-05-23 17:03:07 +0300

    mod render

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,20 +1,32 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox'; // Імпартуем бібліятэку SimpleLightbox
+import 'simplelightbox/dist/simple-lightbox.min.css'; // Імпартуем стылі SimpleLightbox
 
+// Атрымліваем спасылкі на DOM-элементы
 const galleryContainer = document.querySelector('.gallery');
-let lightbox;
+const loaderContainer = document.querySelector('.loader-container');
+const loadMoreButton = document.querySelector('.load-more-button');
 
-export function createGallery(images) {
+let lightboxInstance = null; // Зменная для захоўвання экзэмпляра SimpleLightbox, назва зменена для яснасці
+
+/**
+ * Стварае HTML-разметку для галерэі малюнкаў і дадае яе ў DOM.
+ * Ініцыялізуе або абнаўляе SimpleLightbox.
+ * @param {Array<object>} images - Масіў аб'ектаў малюнкаў з Pixabay API.
+ * @param {boolean} [append=false] - Калі true, дадае новыя выявы да існуючых.
+ * Калі false, ачышчае галерэю перад даданнем.
+ */
+export function createGallery(images, append = false) {
+  // Ствараем HTML-разметку для кожнага малюнка
   const markup = images
     .map(
       ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
+        webformatURL, // URL маленькай версіі выявы
+        largeImageURL, // URL вялікай версіі выявы для SimpleLightbox
+        tags, // Тэгі (выкарыстоўваюцца як alt-тэкст)
+        likes, // Колькасць лайкаў
+        views, // Колькасць праглядаў
+        comments, // Колькасць каментароў
+        downloads, // Колькасць загрузак
       }) => `
             <li class="gallery-item">
                 <a href="${largeImageURL}">
@@ -29,27 +41,69 @@ export function createGallery(images) {
             </li>
         `
     )
-    .join('');
+    .join(''); // Аб'ядноўваем усе радкі ў адзін вялікі радок HTML
 
-  galleryContainer.innerHTML = markup;
-
-  if (lightbox) {
-    lightbox.refresh();
+  if (append) {
+    // Калі append=true, дадаем разметку ў канец існуючай галерэі
+    galleryContainer.insertAdjacentHTML('beforeend', markup);
+    // Калі SimpleLightbox ужо ініцыялізаваны, абнаўляем яго
+    if (lightboxInstance) {
+      lightboxInstance.refresh();
+    }
   } else {
-    lightbox = new SimpleLightbox('.gallery a');
+    // Калі append=false, замяняем усю існуючую разметку новай
+    galleryContainer.innerHTML = markup;
+    // Паколькі мы цалкам замянілі HTML, трэба пераініцыялізаваць SimpleLightbox
+    // Спачатку знішчаем стары экзэмпляр, калі ён ёсць
+    if (lightboxInstance) {
+      lightboxInstance.destroy();
+    }
+    // Затым ствараем новы экзэмпляр SimpleLightbox
+    lightboxInstance = new SimpleLightbox('.gallery a', {
+      captionsData: 'alt', // Выкарыстоўваем атрыбут alt выявы для подпісу ў лайтбоксе
+      captionDelay: 250, // Затрымка перад з'яўленнем подпісу
+    });
   }
 }
 
+/**
+ * Ачышчае змесціва кантэйнера галерэі.
+ * Таксама знішчае экзэмпляр SimpleLightbox, каб прадухіліць праблемы
+ * пры наступнай ініцыялізацыі.
+ */
 export function clearGallery() {
   galleryContainer.innerHTML = '';
+  // Калі экзэмпляр SimpleLightbox існуе, знішчаем яго
+  if (lightboxInstance) {
+    lightboxInstance.destroy(); // Выклікаем метад destroy() для ачысткі SimpleLightbox
+    lightboxInstance = null; // Скідваем зменную, каб яна была null для наступнай ініцыялізацыі
+  }
 }
 
+/**
+ * Паказвае лоадер, дадаючы клас 'is-loading'.
+ */
 export function showLoader() {
-  const loaderContainer = document.querySelector('.loader-container');
   loaderContainer.classList.add('is-loading');
 }
 
+/**
+ * Хавае лоадер, прыбіраючы клас 'is-loading'.
+ */
 export function hideLoader() {
-  const loaderContainer = document.querySelector('.loader-container');
   loaderContainer.classList.remove('is-loading');
+}
+
+/**
+ * Паказвае кнопку "Загрузіць больш", прыбіраючы клас 'is-hidden'.
+ */
+export function showLoadMoreButton() {
+  loadMoreButton.classList.remove('is-hidden');
+}
+
+/**
+ * Хавае кнопку "Загрузіць больш", дадаючы клас 'is-hidden'.
+ */
+export function hideLoadMoreButton() {
+  loadMoreButton.classList.add('is-hidden');
 }
